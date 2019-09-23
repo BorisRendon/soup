e3f538f74216d4504d4f102b861fe5159c1ff7b4
Boris Rendon <boris.rendon@hotmail.com>
2019-09-23 09:48:15 -0600

    Parte CS casi terminada

--- CS.py
+++ CS.py
@@ -15,8 +15,19 @@ soup = BeautifulSoup(r.content, "html.parser")
 ###
 
 # Download FCE logo
+Logo = soup.find("img", {"class": 'fl-photo-img wp-image-500 size-full' })
+strlogo = f"{Logo.get('src')}\n" 
 
 #  GET following <meta>: "title", "description" ("og")
+metatitle = ""
+itemMeta = soup.find('meta', {'property':'og:title'})['content']
+for i in itemMeta:
+    metatitle = f"{itemMeta}"
+
+metaDescription = ""
+itemDescription = soup.find('meta', {'property':'og:description'})['content']
+for description in itemDescription:
+    metaDescription = f"{itemDescription}"
 
 #count all <a>
 ContadorA = soup.findAll('a')
@@ -26,5 +37,8 @@ ContadorDiv = soup.findAll('div')
 #Prints
 print("Titulo CS: \n" , soup.title.string)
 #print("Href: \n" , href)
+print("Logo FCE:\n" , strlogo)
+print("Meta title:\n" ,metatitle)
+print("Meta Description:\n" , metaDescription)
 print("Contador de <a>:\n ", str(len(ContadorA)))
 print("Contador de <div>:\n", str(len(ContadorDiv)))
